cfa04964b7b6e7e485d011ca94c5ddd032e920d3
Mansimar Kaur Jodhka <Mansimar@paybright.com>
2020-09-28 15:29:02 -0400

    Add project files.

---
+++ DatabasePOC/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Login]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [Email] VARCHAR(30) NULL, 
+    [Password] VARCHAR(20) NULL, 
+    [FirstName] VARCHAR(30) NULL, 
+    [LastName] VARCHAR(30) NULL, 
+    [Salt] VARCHAR(50) NULL
+)
